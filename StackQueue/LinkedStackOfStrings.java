a20487b6b127e24319c4766cddd38844eb430c1f
Phuoc Nguyen <andy.314.dn@gmail.com>
2016-10-20 20:02:18 +0700

    stack and queue

---
+++ StackQueue/LinkedStackOfStrings.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * class LinkedStackOfStrings
+ * boolean isEmpty()
+ * void push(String item)
+ * String pop()
+ *
+ * Stack considerations:
+ * 1/ stack overflow - stack underflow
+ * 2/ null items
+ * 3/ loitering
+ *
+ */
+
