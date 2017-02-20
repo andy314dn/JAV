bc847d42967ce9596023807097157e6a452e5ec7
Phuoc Nguyen <andy.314.dn@gmail.com>
2017-02-20 07:50:50 +0700

    union-find-api

---
+++ union-find/uf_api.java
@@ -0,0 +1,10 @@
+/**
+ * public class UF
+ * -----------------------------------------------
+ * UF(int N) => initialize union-find data structure with N objects(0 to N-1)
+ * void union(int p, int q) => add connection between p and q
+ * boolean connected(int p, int q) => are p and q in the same component?
+ * _optional_ int find(int p) => component identifier for p(0 to N-1)
+ * _optional_ int count() => number of components
+ */
+ 
